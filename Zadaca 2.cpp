e3990d1cbec5062eb3ad985df5ce5817473dd447
AnesCehajic <91131302+AnesCehajic@users.noreply.github.com>
2021-09-21 14:10:09 +0200

    Add files via upload

---
+++ Zadaca 2.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+using namespace std;
+int main();
+void findseason(int M)
+{
+    switch(M)
+}
+case 12;
+case 1;
+case 2;
+cout<<("Zima je");
+break;
+case 3;
+case 4;
+case 5;
+cout<<("Proljeće je");
+break;
+case 6;
+case 7;
+case 8;
+cout<<("Ljeto je");
+break;
+case 9;
+case 10;
+case 11;
+cout<<("Jesen je");
+break;
+default;
+cout<<("Mjesec je invalidan,napisi mjesec izmedju (1-12");
+return 0;
+1 zadatak
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{   int YYYY,MM,DD;
+    cout<<"Napisi datum rođenja:"<<endl;
+    cin>>YYYY>>MM>>DD;
+    cout<<"Napiši današnji datum"<<endl;
+    int YYYYY,MMM,DDD;
+    cin>>YYYYY>>MMM>>DDD;
+    
+    //if(MM==MMM)
+    if (MM==MMM && (DDD>=DD)){cout<<"starost u godinama:"<<YYYYY-YYYY<<endl;
+    cout<<"Godina u mjesecu je:"<<((YYYYY-YYYY)*12)<<endl;}
+    else if(MM<MMM){cout<<"starost u godinama: "<<YYYYY-YYYY-1<<endl;
+    cout<<"Godina u mjesecu je:"<<((YYYYY-YYYY)*12)-MM+MMM<<endl;}
+    
+    system("PAUSE");
+    return 0;
+}
+
+    2 zadatak
+
+    
+
+
+
+
+
+    #include <iostream>
+    using namespace std;
+    int main;
+    {
+        int num;
+        printf<<("Napiši neki broj");
+        scanf<<"$d"$num");
+        if<<("num%3==0");
+        if<<("num%5==0");
+
+        {
+            printf("%d je djeljiv sa 3",num);
+        }
+        else
+            printf("%d nije djeljiv sa 3",num);
+        }
+        break;
+        }
+            printf("%d je djeljiv sa 5",num);
+        else
+            printf("%d nije djeljiv sa 5",num);
+        }
+        return 0;
+    
+    {
+        3 zadatak
+
+
+
+
+
+
+
+
